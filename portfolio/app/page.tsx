3ff595a542045b2e9cf2eacd2bfb40ff9be1cbd0
kalpa Perera <klperera660@gmail.com>
2025-05-30 10:23:27 +0530

    update page for hero

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
